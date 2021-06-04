20fa63f8aca9bfcfa654f4c0c61ecc6a06bef6b3
Abdul Mohaimen <mohaimen707@gmail.com>
2021-06-05 02:01:45 +0600

    category design adds

---
+++ user/my-app/src/components/home/HomeTop.js
@@ -0,0 +1,51 @@
+import React, {Component, Fragment} from 'react';
+import {Container, Col, Row} from "react-bootstrap";
+import MegaMenu from "./MegaMenu";
+import Slider from "./Slider";
+
+class HomeTop extends Component {
+
+    constructor(props) {
+        super(props);
+        this.megaMenu = this.megaMenu.bind(this);
+    }
+
+    componentDidMount() {
+        this.megaMenu();
+    }
+
+    megaMenu() {
+        var acc = document.getElementsByClassName('accordion');
+        var accNumber = acc.length;
+        var i;
+        for (i=0; i< accNumber; i++) {
+            acc[i].addEventListener("click", function () {
+                this.classList.toggle('active');
+                var panel = this.nextElementSibling;
+                if(panel.style.maxHeight) {
+                    panel.style.maxHeight = null;
+                } else {
+                    panel.style.maxHeight = panel.scrollHeight+ "px";
+                }
+            })
+        }
+    }
+    render() {
+        return (
+            <Fragment>
+                <Container className="p-0 -0" fluid={true}>
+                    <Row>
+                        <Col lg={3} md={3} sm={12}>
+                            <MegaMenu />
+                        </Col>
+                        <Col lg={9} md={9} sm={12}>
+                            <Slider />
+                        </Col>
+                    </Row>
+                </Container>
+            </Fragment>
+        );
+    }
+}
+
+export default HomeTop;
--- user/my-app/src/components/home/MegaMenu.js
+++ user/my-app/src/components/home/MegaMenu.js
@@ -3,8 +3,132 @@ import React, {Component} from 'react';
 class MegaMenu extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="accordionMenuDiv">
+                <div className="accordionMenuDivInside">
+                    <button className="accordion">
+                       <i className="fa fa-list-alt"></i>
+                        Men's Clothing
+                    </button>
+                    <div className="panel">
+                        <ul>
+                            <li>
+                                <a href="#" className="accordionItem">Mem Shirt</a>
+                            </li>
+                            <li>
+                                <a href="#" className="accordionItem">Mem Shirt</a>
+                            </li>
+                            <li>
+                                <a href="#" className="accordionItem">Mem Shirt</a>
+                            </li>
+                            <li>
+                                <a href="#" className="accordionItem">Mem Shirt</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <button className="accordion">
+                        <i className="fa fa-list-alt"></i>
+                        Men's Clothing
+                    </button>
+                    <div className="panel">
+                        <ul>
+                            <li>
+                                <a href="#" className="accordionItem">Mem Shirt</a>
+                            </li>
+                            <li>
+                                <a href="#" className="accordionItem">Mem Shirt</a>
+                            </li>
+                            <li>
+                                <a href="#" className="accordionItem">Mem Shirt</a>
+                            </li>
+                            <li>
+                                <a href="#" className="accordionItem">Mem Shirt</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <button className="accordion">
+                        <i className="fa fa-list-alt"></i>
+                        Women's Clothing
+                    </button>
+                    <div className="panel">
+                        <ul>
+                            <li>
+                                <a href="#" className="accordionItem">Mem Shirt</a>
+                            </li>
+                            <li>
+                                <a href="#" className="accordionItem">Mem Shirt</a>
+                            </li>
+                            <li>
+                                <a href="#" className="accordionItem">Mem Shirt</a>
+                            </li>
+                            <li>
+                                <a href="#" className="accordionItem">Mem Shirt</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <button className="accordion">
+                        <i className="fa fa-list-alt"></i>
+                        Women's Clothing
+                    </button>
+                    <div className="panel">
+                        <ul>
+                            <li>
+                                <a href="#" className="accordionItem">Mem Shirt</a>
+                            </li>
+                            <li>
+                                <a href="#" className="accordionItem">Mem Shirt</a>
+                            </li>
+                            <li>
+                                <a href="#" className="accordionItem">Mem Shirt</a>
+                            </li>
+                            <li>
+                                <a href="#" className="accordionItem">Mem Shirt</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <button className="accordion">
+                        <i className="fa fa-list-alt"></i>
+                        Women's Clothing
+                    </button>
+                    <div className="panel">
+                        <ul>
+                            <li>
+                                <a href="#" className="accordionItem">Mem Shirt</a>
+                            </li>
+                            <li>
+                                <a href="#" className="accordionItem">Mem Shirt</a>
+                            </li>
+                            <li>
+                                <a href="#" className="accordionItem">Mem Shirt</a>
+                            </li>
+                            <li>
+                                <a href="#" className="accordionItem">Mem Shirt</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <button className="accordion">
+                        <i className="fa fa-list-alt"></i>
+                        Women's Clothing
+                    </button>
+                    <div className="panel">
+                        <ul>
+                            <li>
+                                <a href="#" className="accordionItem">Mem Shirt</a>
+                            </li>
+                            <li>
+                                <a href="#" className="accordionItem">Mem Shirt</a>
+                            </li>
+                            <li>
+                                <a href="#" className="accordionItem">Mem Shirt</a>
+                            </li>
+                            <li>
+                                <a href="#" className="accordionItem">Mem Shirt</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         );
     }
