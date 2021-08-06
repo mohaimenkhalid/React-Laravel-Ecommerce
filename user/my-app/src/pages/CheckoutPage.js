6f5b3ff41941ba80e270598a05ab456a361ea865
mohaimenkhallid <mohaimen707@gmail.com>
2021-08-07 02:23:25 +0600

    Design checkout page

--- user/my-app/src/pages/CheckoutPage.js
+++ user/my-app/src/pages/CheckoutPage.js
@@ -46,11 +46,65 @@ class CheckoutPage extends Component {
   render() {
 
     if (this.state.isLoading === true) {
-      return <h5>Loading...</h5>
+      return (
+        <div className="row">
+          <div className="col-md-12">
+            <h5>
+              <i className="fa fa-spinner fa-spin mx-2" />
+              Loading...
+            </h5>
+          </div>
+        </div>
+      )
     }
     return (
       <>
-        Checkout
+        <div className="row">
+            <div className="col-md-8">
+              <div className="card">
+                <div className="card-body">
+                  <h5>Shipping Information</h5>
+                  <div className="row mt-3">
+                    <label className="col-12"><strong>Contact</strong></label>
+                    <div className="form-group col-6">
+                      <input type="text" className="form-control" placeholder="Enter Your Name" />
+                    </div>
+                    <div className="form-group col-6">
+                      <input type="text" className="form-control" placeholder="Enter Phone No." />
+                    </div>
+                  </div>
+                  <div className="row">
+                    <label className="col-12"><strong>Address</strong></label>
+                    <div className="form-group col-4">
+                      <select className="form-control">
+                          <option>Select Region</option>
+                          <option>Dhaka</option>
+                      </select>
+                    </div>
+                    <div className="form-group col-4">
+                      <input type="text" className="form-control" placeholder="Enter Your City" />
+                    </div>
+                    <div className="form-group col-4">
+                      <input type="text" className="form-control" placeholder="Enter Your Area" />
+                    </div>
+                    <div className="form-group col-12">
+                      <textarea className="form-control" placeholder="Enter Your Address" />
+                    </div>
+                  </div>
+
+                  <div className="row">
+                    <label className="col-12"><strong>Payment</strong></label>
+                    <div className="form-group col-12">
+                      <select className="form-control">
+                        <option>Cash on Delivery</option>
+                        <option>Stripe</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
 
         { this.HomePageRedirect() }
       </>
